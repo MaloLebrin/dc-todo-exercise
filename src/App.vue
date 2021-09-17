02e8e1c9fac2c50797068a711f6b2e821ab44eae
malolebrin <malolebrin@gmail.com>
2021-09-17 11:56:49 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="app"
+    class="min-h-screen w-screen bg-gray-200 flex items-center justify-center"
+  >
+    <ul class="w-full max-w-md">
+      <!-- <li
+        v-for="user in users"
+        :key="user.id"
+        class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move"
+      >
+        <div class="flex items-center">
+          <img
+            class="w-10 h-10 rounded-full"
+            :src="user.avatar"
+            :alt="user.name"
+          >
+          <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
+        </div>
+        <div class="flex">
+          <button
+            aria-label="Edit user"
+            class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600 cursor-pointer"
+            @click="onEdit(user)"
+          >
+            Editer
+          </button>
+          <button
+            aria-label="Delete user"
+            class="p-1 focus:outline-none ml-2 focus:shadow-outline text-white bg-red-500 rounded hover:text-red-600 hover:border-red hover:border-2 hover:bg-white cursor-pointer"
+            @click="onDelete(user)"
+          >
+            Supprimer
+          </button>
+        </div>
+      </li> -->
+      <ListItem
+        :users="elements"
+        @delete="onDeleteUser"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, defineComponent } from 'vue'
+import ListItem from './components/listItem.vue'
+import users from '../users.json'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ListItem,
+  },
+  setup() {
+    const elements = ref(users)
+    function onDeleteUser(userToDelete) {
+      const newArrayOfUsers = elements.value.filter(user => user.id !== userToDelete.id)
+      elements.value = newArrayOfUsers
+    }
+
+    return {
+      onDeleteUser,
+      elements,
+    }
+  },
+})
 </script>
 
 <style>
---
+++ src/components/listItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <li
+    v-for="user in users"
+    :key="user.id"
+    class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move"
+  >
+    <div class="flex items-center">
+      <img
+        class="w-10 h-10 rounded-full"
+        :src="user.avatar"
+        :alt="user.name"
+      >
+      <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
+    </div>
+    <div class="flex">
+      <button
+        aria-label="Edit user"
+        class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600 cursor-pointer"
+        @click="onEdit(user)"
+      >
+        Editer
+      </button>
+      <button
+        aria-label="Delete user"
+        class="p-1 focus:outline-none ml-2 focus:shadow-outline text-white bg-red-500 rounded hover:text-red-600 hover:border-red hover:border-2 hover:bg-white cursor-pointer"
+        @click="onDelete(user)"
+      >
+        Supprimer
+      </button>
+    </div>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ListItem',
+  props: {
+    users: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(_, { emit }) {
+    function onEdit(user) {
+      emit('edit', user)
+    }
+    function onDelete(user) {
+      emit('delete', user)
+    }
+    return {
+      onEdit,
+      onDelete,
+    }
+  },
+})
+</script>
